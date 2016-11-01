a6e4f4a4aedfe1ed97f06d4271bface0b9802869
Marcos P <ing.marcosp@icloud.com>
2016-11-01 12:49:24 +0100

    REFACTOR::
    I've been working on the class, separate some methods making more readable and clean,
    I add some documentation, almost all the methods are clear, also I'm trying to make it more OOP
    the second constructor automaticly add tha Back option to submenus, create a field called currentMenu
    to separate the diferents menus.

--- src/textmenu/TextMenu.java
+++ src/textmenu/TextMenu.java
@@ -17,19 +17,26 @@ import textmenu.ViewMenu.*;
  */
 public final class TextMenu {
     
-    private static HashMap<String, Runnable> principalMenu;
+    private static HashMap<String, Runnable> principalMenu = new HashMap<>();
+    private static HashMap<String, Runnable> currentMenu = new HashMap<>();
+    private static HashMap<String, Runnable> activityMenu = new HashMap<>();
     private static ViewMenu view = new ViewMenu();
     private static boolean loop = true;
 
     public TextMenu(){
         view = new ViewMenu();
-        principalMenu = new HashMap<>();
         createPrincipalMenu();
-        run(new Runnable() {public void run() {view.principalMenu();}} );
+        currentMenu = principalMenu;
+        run(view::principalMenu);
     }
 
-    public TextMenu(HashMap<String, Runnable> subMenu) {
-        principalMenu = subMenu;
+    public TextMenu(HashMap<String, Runnable> subMenu, Runnable menu, boolean haveGoBack) {
+        currentMenu = subMenu;
+        if (haveGoBack) {
+            activityMenu.put("b", this::goBack);
+            activityMenu.put("B", this::goBack);
+        }
+        run(menu);
     }
 
     /*
@@ -39,48 +46,48 @@ public final class TextMenu {
         principalMenu.put("p", view::showProfile);
         principalMenu.put("P", view::showProfile);
         principalMenu.put("a", new Runnable() { public void run() { 
-                view.showActivity();
-                TextMenu submenu;
-                HashMap<String, Runnable> menu = new HashMap<String, Runnable>();
-                menu.put("m", view::moreDetails);
-                menu.put("M", view::moreDetails);
-                menu.put("b", (Runnable) () -> { loop = false; });
-                menu.put("B", (Runnable) () -> { loop = false; });
-                submenu = new TextMenu(menu);
-                submenu.run(view::activityMenu);
-                loop = true;
-                createPrincipalMenu();
+                runActivityMenu();
             }});
         principalMenu.put("A", new Runnable() { public void run() { 
-                view.showActivity();
-                TextMenu submenu;
-                HashMap<String, Runnable> menu = new HashMap<>();
-                menu.put("m", new Runnable() { public void run() { view.moreDetails();}});
-                menu.put("M", new Runnable() { public void run() { view.moreDetails();}});
-                menu.put("b", new Runnable() { public void run() { loop = false;}});
-                menu.put("B", new Runnable() { public void run() { loop = false;}});
-                submenu = new TextMenu(menu);
-                submenu.run(new Runnable() {public void run() {view.activityMenu();}});
-                loop = true;
-                createPrincipalMenu();
+                runActivityMenu();
             }});
-        principalMenu.put("q", (Runnable) () -> {view.quit();
-                                                 System.exit(0);
-                                            });
-        principalMenu.put("Q", new Runnable() { public void run() { view.quit();
-                                                                    System.exit(0);}});
+        principalMenu.put("q", (Runnable) () -> {quitProgram();});
+        principalMenu.put("Q", (Runnable) () -> {quitProgram();});
     }
-
+    
+    private void createActivityMenu() {
+        activityMenu.put("m", new Runnable() { public void run() { view.moreDetails();}});
+        activityMenu.put("M", new Runnable() { public void run() { view.moreDetails();}});
+    }
+    
+    private void runActivityMenu() {
+        view.showActivity();
+        createActivityMenu();
+        TextMenu submenu = new TextMenu(activityMenu, view::activityMenu, true);
+        loop = true;
+        currentMenu = principalMenu;
+    }
+    
+    
     public void run(Runnable menu) {
         while(loop){
             menu.run();
             String option = new Scanner(System.in).nextLine();
-            if(principalMenu.containsKey(option)){
-                principalMenu.get(option).run();
+            if(currentMenu.containsKey(option)){
+                currentMenu.get(option).run();
             }
             else {
                 view.wrongOption();
             }
         }
     }
+    
+    private void quitProgram() {
+        view.quit();
+        System.exit(0);
+    }
+    private void goBack() {
+        loop = false;
+    }
+    
 }
